b43183d2d7f6ed368a893d73d8155e816f8b1fd1
hsiangtinglin <hsiangting051419@gmail.com>
2024-01-22 16:52:20 -0500

    changes 2

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy('images');
-    eleventyConfig.addPassthroughCopy('styles');
+    eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy("styles.css");
 }
